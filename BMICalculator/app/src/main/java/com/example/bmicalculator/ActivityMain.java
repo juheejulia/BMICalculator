a50f51a110569f8cc2f059e996dbe3aa3568d559
Juhee Kang <juhee.kang@infotiv.se>
2023-10-02 22:46:28 +0200

    xml design done

---
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/ActivityMain.java
@@ -0,0 +1,12 @@
+package com.example.bmicalculator;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityMain extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
