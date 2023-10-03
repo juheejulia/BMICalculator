c6d431ec08bd919ab72833b95a2a7cfdf21cfe06
Juhee Kang <juhee.kang@infotiv.se>
2023-10-03 21:16:53 +0200

    update

--- BMICalculator/app/src/main/java/com/example/bmicalculator/ActivityMain.java
+++ BMICalculator/app/src/main/java/com/example/bmicalculator/ActivityMain.java
@@ -1,12 +1,79 @@
 package com.example.bmicalculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityMain extends AppCompatActivity {
+
+    EditText height, weight;
+    Button calculator;
+    TextView result;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        height = findViewById(R.id.height);
+        weight = findViewById(R.id.weight);
+        calculator = findViewById(R.id.btn_calculate);
+        result = findViewById(R.id.result);
+
+        calculator.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float height = Float.parseFloat(editTextHeight.getText());
+                float weight = Float.parseFloat(editTextWeight.getText());
+
+                calculator();
+                calculateBMI(view);
+            }
+            public void calculator() {
+                calculator.performClick();
+            }
+        });                                                                                                  
     }
+
+    public void calculateBMI(View view) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+            if (height != null && weight != null && !"".equals(weight)) {
+                float heightValue = Float.parseFloat(heightStr) / 100;
+                float weightValue = Float.parseFloat(weightStr);
+
+                float bmi = weightValue / (heightValue * heightValue);
+
+                displayBMI(bmi);
+        }
+    }
+    private void displayBMI(float bmi){
+        String bmiLabel = "";
+
+        if (Float.compare(bmi, 15f) <= 0) {
+            bmiLabel = getString(R.string.very_severely_underweight);
+        } else if (Float.compare(bmi, 15f) > 0 && Float.compare(bmi, 16f) <= 0) {
+            bmiLabel = getString(R.string.severely_underweight);
+        } else if (Float.compare(bmi, 16f) > 0 && Float.compare(bmi, 18.5f) <= 0) {
+            bmiLabel = getString(R.string.underweight);
+        } else if (Float.compare(bmi, 18.5f) > 0 && Float.compare(bmi, 25f) <= 0) {
+            bmiLabel = getString(R.string.normal);
+        } else if (Float.compare(bmi, 25f) > 0 && Float.compare(bmi, 30f) <= 0) {
+            bmiLabel = getString(R.string.overweight);
+        } else if (Float.compare(bmi, 30f) > 0 && Float.compare(bmi, 35f) <= 0) {
+            bmiLabel = getString(R.string.obese_class_i);
+        } else if (Float.compare(bmi, 35f) > 0 && Float.compare(bmi, 40f) <= 0) {
+            bmiLabel = getString(R.string.obese_class_ii);
+        } else {
+            bmiLabel = getString(R.string.obese_class_iii);
+        }
+        bmiLabel = bmi + "\n\n" + bmiLabel;
+        result.setText(bmiLabel);
+        System.out.println(result);
+    }
+
 }
